ca2123909724b747ee3a33f5991a537f713ef028
tocteman <juanfrancisco.vinueza@gmail.com>
2022-09-05 20:16:42 -0500

    Create vrooms.js

---
+++ pages/components/vrooms.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Vrooms = () => {
+  return (
+    <div className="py-4 mx-8">
+      <div>
+        <a href="https://vrooms.ec" target="_blank">
+        <img src="/images/vrooms.jpg" className="mb-2 mx-2 border-2 border-black shadow" alt=""/>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Vrooms
